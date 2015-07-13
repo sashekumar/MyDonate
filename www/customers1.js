02507cbfdadbd9cf81fc0fc247555a8d1b6696aa
sashekumar <sashekumar@gmail.com>
2015-07-13 23:01:51 +0800

    Initial

---
+++ www/customers1.js
@@ -0,0 +1,19 @@
+﻿angular.module('app', ['onsen'])
+
+            .controller('CustomerController', function ($scope, $http) {
+               
+               
+                 
+            })
+
+            .controller('LoginController', function ($scope, $http) {
+                window.localStorage.removeItem("memberCode");
+                this.mcode;
+                this.password;
+                this.pay = function pay() {
+                    menu.setMainPage('customers.html', { closeMenu: true });
+                };
+            })
+
+;
+;
---
+++ www/logins.js
@@ -0,0 +1,4 @@
+﻿angular.module('app', ['onsen'])
+.controller('Controller', ['$scope', function ($scope) {
+    $scope.name = 'Max Karl Ernst Ludwig Planck (April 23, 1858 – October 4, 1947)';
+}]);
